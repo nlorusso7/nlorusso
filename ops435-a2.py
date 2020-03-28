dbc52cc5842b952b5673366f7151f4b8a4190b96
nlorusso7 <31771340+nlorusso7@users.noreply.github.com>
2020-03-28 15:34:41 -0400

    Add files via upload

---
+++ ops435-a2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import time
+import sys
+
+parse = argp.ArguementParser()
+parse.addArgument("F")
+arg = parse.parse_args()
+file1 = open(args.F, 'r')
+
+
+Host = input(" ")
+list = []
+read = file7.readlines()
+for line in read:
+    split[0] == Host:
+    if split[0] == Host;
+        list.append(line)
+    else:
+        pass
+
+
+
+
+dictionary = {}
+for line in list:
+    split = line.split()
+    input_date = (split[3]+' ' + split[7] + ' '+ monName(split[4])+ ' ' + split[5])
+    output_date = (split[9]+ ' ' + split[13] + ' ' + monName(split[10])+' 'split[11])
+    
+    
+    time_tup = (int(time[1]), int(time[2]), (int(time[3]) + 1),0,0,0, nameOfWeek(time[0]), dayOfYear(time[3], time[2], time[1]), -1)
+
+    time = input_date.split()
+    Hour = split[6].split(':')[0]
+    Minute = split[6].split(':')[1]
+    Second = split[].split(':')[2]
+    input_t = (int(time[1]), int(time[2]), (int(time[3]) + 1),int(Hour), int(Minute), int(Second), nameOfWeek(time[3], time[2], time[1]), -1)
+
+
+    time = output_date.split()
+    Hour = split[6].split(':')[0]
+    Minute = split[6].split(':')[1]
+    Second = split[].split(':')[2]
+    output_t = = (int(time[1]), int(time[2]), (int(time[3]) + 1),int(Hour), int(Minute), int(Second), nameOfWeek(time[3], time[2], time[1]), -1)
+
+
+
+
+    time_obj
+
+
+
+#if __name__ == '__main__':
+
+
+
+
